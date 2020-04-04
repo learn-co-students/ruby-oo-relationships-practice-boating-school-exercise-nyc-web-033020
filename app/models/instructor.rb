8fa2595006122a532e708c471c02fe3310810c3f
swapman972 <jordan.melidor@hotmail.fr>
2020-04-04 14:37:59 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,31 @@
 class Instructor
-
+    attr_reader :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def pass_student(student, test_name)
+        test_passed = "passed"
+        BoatingTest.all.select do |key|
+            if key.student == student && key.test_name == test_name
+                key.test_status = test_passed
+            else
+                BoatingTest.new(student, test_name, test_passed, self)
+            end
+        end
+    end
+    def fail_student
+        test_failed = "failed"
+        BoatingTest.all.select do |key|
+            if key.student == student && key.test_name == test_name
+                key.test_status = test_failed
+            else
+                BoatingTest.new(student, test_name, test_failed, self)
+            end
+        end
+    end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,28 @@
+require 'pry'
 class Student
-
+    attr_reader :name
+    @@all = []
+    def initialize (name)
+        @name = name
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+    def find_student(name)
+        Student.all.select {|key| key.name == name}
+    end
+    def grade_percentage
+        passed_test = []
+        BoatingTest.all.select do |key| 
+            if key.test_status == "passed" && key.student == self
+                passed_test << key
+            end
+        end
+        all_test = BoatingTest.all.select {|key| key.student == self}
+        percentage = (passed_test.count * 1.0) / (all_test.count * 1.0)
+    end
 end
