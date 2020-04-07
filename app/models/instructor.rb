ef0f72b228a00710413c031c1a4532ea80a2ddbc
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-04-06 21:44:40 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,44 @@
 class Instructor
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        Instructor.all << self
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def add_boating_test(student, test_name, test_status)
+        BoatingTest.new(student, test_name, test_status, self)
+    end
+
+    def boating_tests
+        BoatingTest.all.select { |tests| tests.instructor == self }
+    end
+    
+    def pass_student(student, test_name)
+        if boating_tests
+            test_to_change = boating_tests.find { |tests| tests.test_name }
+            test_to_change.test_status = "Pass"
+        else
+            add_boating_test(student, test_name, "Pass")
+        end
+    end
+
+    def fail_student(student, test_name)
+        if boating_tests
+            test_to_change = boating_tests.find { |tests| tests.test_name }
+            test_to_change.test_status = "Fail"
+        else
+            add_boating_test(student, test_name, "Fail")
+        end
+    end
+
+
 end
+ 
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,25 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+#[instructor]--<-[boating test]->--[student]
+
+tashawn = Instructor.new("Tashawn")
+ian = Instructor.new("Ian")
+matt = Instructor.new("Matt")
+
+steve = Student.new("Steve")
+yo = Student.new("Yo")
+edgar = Student.new("Edgar")
+michelle = Student.new("Michelle")
+mike = Student.new("Mike")
+
+steve.add_boating_test("Parking", "Pass", tashawn)
+steve.add_boating_test("Turning", "Pass", ian)
+steve.add_boating_test("Signaling", "Fail", matt)
+steve.add_boating_test("U-Turn", "Pass", matt)
+steve.add_boating_test("Traffic", "Fail", tashawn)
+steve.add_boating_test("Braking", "Pass", ian)
+ 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
